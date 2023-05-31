067974b6373569db57026e0bd2215e3e4d8b16c2
George Machibya <gman2them@gmail.com>
2023-06-01 00:33:24 +0300

    good changes

--- dtree/settings.py
+++ dtree/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 #STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
